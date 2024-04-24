2e2e0bc2534501a28bbf9cb9c44bf30f54b55c97
Jacob Bahn <jacobabahn@gmail.com>
2024-04-24 12:54:27 -0700

    Simplified orders' handler functions

--- server/handlers/orders.ts
+++ server/handlers/orders.ts
@@ -7,27 +7,22 @@ const supabase = createClient()
 
 export const getOrders = async () => {
     return await supabase.from("Orders").select("*")
-    // return { data: orders.data, error: orders.error }
 }
 
 export const getOrderById = async (id: string) => {
-    const order = await supabase.from("Orders").select("*").eq("id", id)
-    return { data: order.data, error: order.error }
+    return await supabase.from("Orders").select("*").eq("id", id)
 }
 
 export const getOrderByUserId = async (userId: string) => {
-    const order = await supabase.from("Orders").select("*").eq("user_id", userId)
-    return { data: order.data, error: order.error }
+    return await supabase.from("Orders").select("*").eq("user_id", userId)
 }
 
 export const createOrder = async (order: Partial<Order>) => {
-    const newOrder = await supabase.from("Orders").insert([order]).select()
-    return { data: newOrder.data, error: newOrder.error }
+    return await supabase.from("Orders").insert([order]).select()
 }
 
 export const updateOrder = async (id: string, order: Partial<Order>) => {
-    const updatedOrder = await supabase.from("Orders").update(order).eq("id", id).select()
-    return { data: updatedOrder.data, error: updatedOrder.error }
+    return await supabase.from("Orders").update(order).eq("id", id).select()
 }
 
 export const deleteOrder = async (id: string) => {
