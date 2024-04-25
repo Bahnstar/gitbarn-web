e902ca6718fceb64168ca5088eee7fbe104d3ee1
Jacob Bahn <jacobabahn@gmail.com>
2024-04-25 13:58:55 -0700

    Added return types to order and product handlers

--- server/handlers/orders.ts
+++ server/handlers/orders.ts
@@ -2,29 +2,37 @@
 
 import { Order } from "@/types/order"
 import { createClient } from "@/utils/supabase/server"
+import { PostgrestSingleResponse } from "@supabase/supabase-js"
 
 const supabase = createClient()
 
-export const getOrders = async () => {
+export const getOrders = async (): Promise<PostgrestSingleResponse<Order[]>> => {
     return await supabase.from("Orders").select("*")
 }
 
-export const getOrderById = async (id: string) => {
+export const getOrderById = async (id: string): Promise<PostgrestSingleResponse<Order[]>> => {
     return await supabase.from("Orders").select("*").eq("id", id)
 }
 
-export const getOrderByUserId = async (userId: string) => {
+export const getOrderByUserId = async (
+    userId: string,
+): Promise<PostgrestSingleResponse<Order[]>> => {
     return await supabase.from("Orders").select("*").eq("user_id", userId)
 }
 
-export const createOrder = async (order: Partial<Order>) => {
+export const createOrder = async (
+    order: Partial<Order>,
+): Promise<PostgrestSingleResponse<Order[]>> => {
     return await supabase.from("Orders").insert([order]).select()
 }
 
-export const updateOrder = async (id: string, order: Partial<Order>) => {
+export const updateOrder = async (
+    id: string,
+    order: Partial<Order>,
+): Promise<PostgrestSingleResponse<Order[]>> => {
     return await supabase.from("Orders").update(order).eq("id", id).select()
 }
 
-export const deleteOrder = async (id: string) => {
-    return await supabase.from("Orders").delete().eq("id", id)
+export const deleteOrder = async (id: string): Promise<void> => {
+    await supabase.from("Orders").delete().eq("id", id)
 }
--- server/handlers/products.ts
+++ server/handlers/products.ts
@@ -2,29 +2,37 @@
 
 import { Product } from "@/types/product"
 import { createClient } from "@/utils/supabase/server"
+import { PostgrestSingleResponse } from "@supabase/supabase-js"
 
 const supabase = createClient()
 
-export const getProducts = async () => {
+export const getProducts = async (): Promise<PostgrestSingleResponse<Product[]>> => {
     return await supabase.from("Products").select("*")
 }
 
-export const getProductsById = async (id: string) => {
+export const getProductsById = async (id: string): Promise<PostgrestSingleResponse<Product[]>> => {
     return await supabase.from("Products").select("*").eq("id", id)
 }
 
-export const getProductsByTitle = async (title: string) => {
+export const getProductsByTitle = async (
+    title: string,
+): Promise<PostgrestSingleResponse<Product[]>> => {
     return await supabase.from("Products").select("*").eq("title", title)
 }
 
-export const createProduct = async (product: Product) => {
+export const createProduct = async (
+    product: Product,
+): Promise<PostgrestSingleResponse<Product[]>> => {
     return await supabase.from("Products").insert([product]).select()
 }
 
-export const updateProduct = async (id: string, product: Partial<Product>) => {
+export const updateProduct = async (
+    id: string,
+    product: Partial<Product>,
+): Promise<PostgrestSingleResponse<Product[]>> => {
     return await supabase.from("Products").update(product).eq("id", id).select()
 }
 
-export const deleteProduct = async (id: string) => {
-    return await supabase.from("Products").delete().eq("id", id)
+export const deleteProduct = async (id: string): Promise<void> => {
+    await supabase.from("Products").delete().eq("id", id)
 }
