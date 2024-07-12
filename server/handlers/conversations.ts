a295e200f363f54b8169bd8f20d54538b32f859d
Miro Manestar <miroimanestar@gmail.com>
2024-07-11 22:56:41 -0400

    Minor changes

--- server/handlers/conversations.ts
+++ server/handlers/conversations.ts
@@ -4,27 +4,29 @@ import { Conversation } from "@/types/conversation"
 import { createClient } from "@/utils/supabase/server"
 import { PostgrestSingleResponse } from "@supabase/supabase-js"
 
-const supabase = createClient()
-
 export const getConversations = async (): Promise<PostgrestSingleResponse<Conversation[]>> => {
+  const supabase = createClient()
   return await supabase.from("Conversations").select("*")
 }
 
 export const getConversationsByCustomerId = async (
   userId: string,
 ): Promise<PostgrestSingleResponse<Conversation[]>> => {
+  const supabase = createClient()
   return await supabase.from("Conversations").select("*").eq("customer_id", userId)
 }
 
 export const getConversationsBySupportId = async (
   userId: string,
 ): Promise<PostgrestSingleResponse<Conversation[]>> => {
+  const supabase = createClient()
   return await supabase.from("Conversations").select("*").eq("support_id", userId)
 }
 
 export const getLastActiveConversationByCustomerId = async (
   userId: string,
 ): Promise<PostgrestSingleResponse<Conversation>> => {
+  const supabase = createClient()
   return await supabase
     .from("Conversations")
     .select("*")
@@ -37,6 +39,7 @@ export const getLastActiveConversationByCustomerId = async (
 export const getLastActiveConversationBySupportId = async (
   userId: string,
 ): Promise<PostgrestSingleResponse<Conversation>> => {
+  const supabase = createClient()
   return await supabase
     .from("Conversations")
     .select("*")
@@ -49,12 +52,14 @@ export const getLastActiveConversationBySupportId = async (
 export const getConversationById = async (
   id: string,
 ): Promise<PostgrestSingleResponse<Conversation>> => {
+  const supabase = createClient()
   return await supabase.from("Conversations").select("*").eq("id", id).limit(1).single()
 }
 
 export const createConversation = async (
   conversation: Conversation,
 ): Promise<PostgrestSingleResponse<Conversation>> => {
+  const supabase = createClient()
   return await supabase.from("Conversations").insert([conversation]).select().limit(1).single()
 }
 
@@ -62,6 +67,7 @@ export const updateConversation = async (
   id: string,
   conversation: Partial<Conversation>,
 ): Promise<PostgrestSingleResponse<Conversation>> => {
+  const supabase = createClient()
   return await supabase
     .from("Conversations")
     .update(conversation)
@@ -72,5 +78,6 @@ export const updateConversation = async (
 }
 
 export const deleteConversation = async (id: string): Promise<void> => {
+  const supabase = createClient()
   await supabase.from("Conversations").delete().eq("id", id)
 }
