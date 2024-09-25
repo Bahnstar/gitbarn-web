e80ae9b8717cb15867a15da8c339cb32c549ccdd
Jacob Bahn <jacobabahn@gmail.com>
2024-09-25 11:18:49 -0700

    Fixed landing page code format

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,8 @@ export default async function Index() {
     {
       title: "Online Orders",
       image: "https://gitbarn.com/wp-content/uploads/2018/07/GIT-pictures-at-3080.jpg",
-      description: "Competitive Pricing | Outstanding Customer Service | Friendly Drivers | Clean Trucks",
+      description:
+        "Competitive Pricing | Outstanding Customer Service | Friendly Drivers | Clean Trucks",
     },
     {
       title: "Online Orders",
@@ -33,16 +34,15 @@ export default async function Index() {
       title: "Online Orders",
       image: "https://gitbarn.com/wp-content/uploads/2018/06/shutterstock_554368408.jpg",
       description: "Even horses recommend us!",
-    }
+    },
   ]
 
-
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <main className="flex flex-col gap-28">
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
           <div className="container flex h-14 items-center">
-            <div className="flex items-center gap-2 mx-6">
+            <div className="mx-6 flex items-center gap-2">
               <img
                 src="https://gitbarn.com/wp-content/uploads/2018/06/gitbarn-logo.png"
                 alt="Git Barn Logo"
@@ -51,42 +51,65 @@ export default async function Index() {
             </div>
             <div className="mr-4 hidden md:flex">
               <nav className="flex items-center space-x-6 text-sm font-medium">
-                <a className="transition-colors hover:text-foreground/80 text-foreground/60" href="#services">Services</a>
-                <a className="transition-colors hover:text-foreground/80 text-foreground/60" href="#about">About</a>
-                <a className="transition-colors hover:text-foreground/80 text-foreground/60" href="#contact">Contact</a>
-                <a className="transition-colors hover:text-foreground/80 text-foreground/60" href="login">Login</a>
+                <a
+                  className="text-foreground/60 transition-colors hover:text-foreground/80"
+                  href="#services"
+                >
+                  Services
+                </a>
+                <a
+                  className="text-foreground/60 transition-colors hover:text-foreground/80"
+                  href="#about"
+                >
+                  About
+                </a>
+                <a
+                  className="text-foreground/60 transition-colors hover:text-foreground/80"
+                  href="#contact"
+                >
+                  Contact
+                </a>
+                <a
+                  className="text-foreground/60 transition-colors hover:text-foreground/80"
+                  href="login"
+                >
+                  Login
+                </a>
               </nav>
             </div>
           </div>
         </header>
-        <section className="flex flex-col justify-center items-center w-3/5 m-auto">
+        <section className="m-auto flex w-3/5 flex-col items-center justify-center">
           <div className="flex flex-col items-center gap-4">
             <BlurFade delay={0.25}>
               <h1 className="text-6xl font-bold">Welcome to Git Barn</h1>
             </BlurFade>
-            <BlurFade delay={.5}>
-              <h2 className="text-4xl">The <span className="font-semibold text-green-400">#1</span>  horse manure solution in South Florida</h2>
+            <BlurFade delay={0.5}>
+              <h2 className="text-4xl">
+                The <span className="font-semibold text-green-400">#1</span> horse manure solution
+                in South Florida
+              </h2>
             </BlurFade>
           </div>
         </section>
-        <section className="m-auto w-4/6 h-[50vh]">
-          <BlurFade delay={.75} className="flex flex-col items-center h-full">
+        <section className="m-auto h-[50vh] w-4/6">
+          <BlurFade delay={0.75} className="flex h-full flex-col items-center">
             <Swiper data={swiperData} />
           </BlurFade>
         </section>
         <section className="m-auto w-3/5">
           <BlurFade delay={1}>
-            <div className="flex flex-row items-center gap-12 w-full">
-              <div className="flex-1 flex flex-col gap-4">
+            <div className="flex w-full flex-row items-center gap-12">
+              <div className="flex flex-1 flex-col gap-4">
                 <h2 className="text-4xl font-semibold">Git Barn Manure Removal</h2>
                 <p className="flex-1 text-2xl">
-                  Serving Palm Beach County, we provide eco-friendly manure removal services
-                  along with premium yellow pine wood shavings for stalls. Our professional team
-                  is committed to delivering exceptional service, quality products, and reliable
+                  Serving Palm Beach County, we provide eco-friendly manure removal services along
+                  with premium yellow pine wood shavings for stalls. Our professional team is
+                  committed to delivering exceptional service, quality products, and reliable
                   results—all with a focus on efficiency and customer satisfaction.
                 </p>
                 <div className="flex flex-col gap-2 text-lg">
-                  <p className="text-2xl font-medium m-auto">We Service:</p>
+                  <p className="m-auto text-2xl font-medium">We Service:</p>
                   <div className="grid grid-cols-2 gap-2 text-lg">
                     <p className={serviceStyle}>Residential Properties</p>
                     <p className={serviceStyle}>Farms with Residential Properties</p>
@@ -95,9 +118,9 @@ export default async function Index() {
                   </div>
                 </div>
               </div>
-              <div className="flex-1 relative">
+              <div className="relative flex-1">
                 <HeroVideoDialog
-                  className="dark:hidden block"
+                  className="block dark:hidden"
                   animationStyle="from-center"
                   videoSrc="https://www.youtube.com/embed/T0GKop_lgEY?si=hpvXvcVUiStcPnge"
                   thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
@@ -108,14 +131,18 @@ export default async function Index() {
           </BlurFade>
         </section>
         <section className="flex flex-col items-center gap-10">
-          <div className="flex flex-row justify-center items-center gap-8 w-3/6 m-auto">
-            <Iphone15Pro className="h-[50vh]" src={"https://i.ibb.co/kGW15k6/photo-2024-09-12-17-10-14.jpg"} />
-            <div className="flex-1 flex flex-col items-center h-full gap-8">
+          <div className="m-auto flex w-3/6 flex-row items-center justify-center gap-8">
+            <Iphone15Pro
+              className="h-[50vh]"
+              src={"https://i.ibb.co/kGW15k6/photo-2024-09-12-17-10-14.jpg"}
+            />
+            <div className="flex h-full flex-1 flex-col items-center gap-8">
               <h2 className="text-4xl font-semibold">Git Barn Software Portal</h2>
-              <p className="text-xl">We created a custom software portal for our customers to easily receive
-                support, view orders, and check documents related to billing. The portal is easy to use and provides a seamless
-                experience for our customers. You can access the portal by clicking the
-                Login button on the top menu.
+              <p className="text-xl">
+                We created a custom software portal for our customers to easily receive support,
+                view orders, and check documents related to billing. The portal is easy to use and
+                provides a seamless experience for our customers. You can access the portal by
+                clicking the Login button on the top menu.
               </p>
               <WordRotate
                 className="text-4xl font-bold text-black dark:text-white"
