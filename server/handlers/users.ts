83dd34eb1bc1076b91256031e868d770133abf6f
Jacob Bahn <jacobabahn@gmail.com>
2024-10-15 12:22:04 -0700

    Remove unecessary console.log

--- server/handlers/users.ts
+++ server/handlers/users.ts
@@ -5,7 +5,6 @@ import { cache } from "react"
 
 export const getCurrentUser = cache(async () => {
   const supabase = createClient()
-  console.log("getting user")
 
   return await supabase.auth.getUser()
 })
