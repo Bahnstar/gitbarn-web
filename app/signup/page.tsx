92bb9aca000f2d75e67dc6446c63d71fe8742d5f
Miro Manestar <miroimanestar@gmail.com>
2025-03-04 19:25:06 -0500

    Fixed password reset flow

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -30,7 +30,7 @@ export default async function SignupPage() {
           <Form className="md:col-span-2" action={signup} showSubmitButton={false}>
             <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:max-w-xl sm:grid-cols-6">
               <div className="sm:col-span-3">
-                <label htmlFor="first-name" className="block text-sm font-medium leading-6 ">
+                <label htmlFor="first-name" className="block text-sm leading-6 font-medium">
                   First name
                 </label>
                 <div className="mt-2">
@@ -40,13 +40,13 @@ export default async function SignupPage() {
                     id="first-name"
                     required
                     autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:ring-inset sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-                <label htmlFor="last-name" className="block text-sm font-medium leading-6 ">
+                <label htmlFor="last-name" className="block text-sm leading-6 font-medium">
                   Last name
                 </label>
                 <div className="mt-2">
@@ -56,13 +56,13 @@ export default async function SignupPage() {
                     id="last-name"
                     required
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:ring-inset sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div className="col-span-full">
-                <label htmlFor="email" className="block text-sm font-medium leading-6 ">
+                <label htmlFor="email" className="block text-sm leading-6 font-medium">
                   Email address
                 </label>
                 <div className="mt-2">
@@ -72,13 +72,13 @@ export default async function SignupPage() {
                     type="email"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div className="col-span-full">
-                <label htmlFor="password" className="block text-sm font-medium leading-6">
+                <label htmlFor="password" className="block text-sm leading-6 font-medium">
                   Password
                 </label>
                 <input
@@ -87,12 +87,12 @@ export default async function SignupPage() {
                   type="password"
                   minLength={6}
                   required
-                  className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
+                  className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
                 />
               </div>
 
               <div className="col-span-full">
-                <label htmlFor="confirm" className="block text-sm font-medium leading-6">
+                <label htmlFor="confirm" className="block text-sm leading-6 font-medium">
                   Confirm Password
                 </label>
                 <input
@@ -100,14 +100,14 @@ export default async function SignupPage() {
                   name="confirm"
                   type="password"
                   required
-                  className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
+                  className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <button
               type="submit"
-              className="mt-8 w-full rounded-md bg-green-600 px-3 py-4 text-sm font-semibold text-white shadow-xs hover:bg-green-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              className="mt-8 w-full rounded-md bg-green-600 px-3 py-4 text-sm font-semibold text-white shadow-xs hover:bg-green-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
             >
               Sign Up
             </button>
